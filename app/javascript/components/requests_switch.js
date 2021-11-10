fd93a28dd7bdb6c8d1b7828411281402b4495112
Carlos Henrique Moreira <carlos.h.moreira@ibge.gov.br>
2021-11-10 16:05:05 -0300

    caique-view-manager-requests-enhanced

---
+++ app/javascript/components/requests_switch.js
@@ -0,0 +1,9 @@
+// here we hide or not the unselected requests by its status
+
+// document.querySelectorAll("button").forEach((button) => {
+//   button.addEventListener("click", (event) => {
+//     console.log(event);
+//     const stat = documento.getElementByTagName("td").innerText;
+//     console.log(stat);
+//   });
+// });
