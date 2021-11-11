6221b08d3920f7625509c2cfd513af9dc92eddba
Henrique Luna <jhmluna@gmail.com>
2021-11-11 16:17:47 -0300

    Reduced number of views

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -9,13 +9,12 @@ class RequestsController < ApplicationController
       else
         @requests = policy_scope(Request.where(status: params[:query]))
       end
+      render action: "index_manager" and return
     when "tech"
       @requests = policy_scope(Request.where(tech: nil, location: current_user.location).or(Request.where(tech: current_user, location: current_user.location)))
     when "citizen"
       @requests = policy_scope(Request.where(citizen: current_user))
     end
-    # Find the correct index view to render based on user role
-    render view_for_user(:index)
   end
 
   def show
