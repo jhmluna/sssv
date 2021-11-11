ada745af0d127ce9e005f6273828cfc0d2e5c9de
Henrique Luna <jhmluna@gmail.com>
2021-11-11 12:28:21 -0300

    New home page for user

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,16 +2,6 @@ class RequestsController < ApplicationController
   before_action :set_request, only: %i[get conclude show edit update]
 
   def index
-    # case current_user.role
-    # when "manager"
-    #   @requests = policy_scope(Request.where(location: current_user.location))
-    # when "tech"
-    #   @requests = policy_scope(Request.where(tech: nil, location: current_user.location).or(Request.where(tech: current_user, location: current_user.location)))
-    # when "citizen"
-    #   @requests = policy_scope(Request.where(citizen: current_user))
-    # end
-    # # Find the correct index view to render based on user role
-    # render view_for_user(:index)
     case current_user.role
     when "manager"
       if params[:query].blank?
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Database admin route
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  # Authentication route
+  devise_for :users
+
+  # Home and user show
+  root to: 'pages#home'
+  resources :pages, only: %i[index show]
+
+  # Request pages
   resources :requests, except: :destroy
   # Update requests
   patch 'requests/get/:id', to: 'requests#get', as: :request_get
@@ -10,8 +19,9 @@ Rails.application.routes.draw do
   patch 'requests/conclude/:id', to: 'requests#conclude', as: :request_conclude
 
   # Redirect to index page on successful sign in
-  get '/user' => "requests#index", :as => :user_root
+  get '/user' => "pages#index"
 
+  # Blazer dashboard pages
   authenticate :user, ->(user) { user.admin? || user.role == 'manager' } do
     mount Blazer::Engine, at: "blazer"
   end
